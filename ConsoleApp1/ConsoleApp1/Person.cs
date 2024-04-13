39cc5204a850c816d25a556e224b746992635030
jakubkodajek <jakubkodajek@seznam.cz>
2024-04-13 15:44:00 +0200

    Add files to repository

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        // Properties
+        public string name;
+        public float height;
+        public float weight;
+
+        // Constructor
+        public Person(string name,float height, float weight)
+        {
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        //Print the information of a person
+        public void printInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Weight: " + weight);
+            Console.WriteLine("BMI: {0:0.00}",getBMI(weight, height));
+            Console.WriteLine("Classification: {0} \n", classification(getBMI(weight, height)));
+        }
+
+        // Get the BMI of a person
+        public double getBMI(float weight, float height)
+        {
+            return (weight / ((height/100) * (height / 100)));
+        }
+
+        // Get the classification of a person
+        string classification(double bmi)
+        {
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            else if (bmi < 25)
+            {
+                return "Normal";
+            }
+            else if (bmi < 30)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obese";
+            }
+        }
+    }   
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of people
+            List<Person> myPeople = new List<Person>();
+            while (true)
+            {
+                Console.WriteLine(" \"ADD\" to add a new user to the list, \"PRINT\" to show the information of all added people, \"PERSON'S NAME\" to show their information, \"EXIT\" to leave");
+                string input = Console.ReadLine();
+                Console.Write("\n");
+                // Exit the program
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                // Add a new person
+                } else if (input.Equals("add", StringComparison.OrdinalIgnoreCase))
+                {
+                    myPeople.Add(getInfo());
+                    Console.Write("\n");
+
+                // Print the information of all people
+                } else if (input.Equals("print", StringComparison.OrdinalIgnoreCase))
+                {
+                    for (int counter = 0; counter <myPeople.Count; counter++)
+                    {
+                        myPeople[counter].printInfo();
+                    }
+                }
+                else
+                {
+                    // Try to find the person by name
+                    Person foundPerson = myPeople.Find(person => person.name.Equals(input, StringComparison.OrdinalIgnoreCase));
+
+                    // If the person is found, print their information
+                    if (foundPerson != null)
+                    {
+                        foundPerson.printInfo();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                        Console.Write("\n");
+                    }
+                }
+            }
+
+            // Get the information of a person
+            Person getInfo()
+            {
+                Console.WriteLine("Enter name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter height: ");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter weight: ");
+                float weight = float.Parse(Console.ReadLine());
+                return new Person(name, height, weight);
+            }
+        }
+    }
+}
